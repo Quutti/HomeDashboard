3f34751879698aee0b312ea67b863fac6a351150
Jokke Liikkanen <quutticode@gmail.com>
2017-10-15 00:23:50 +0300

    Refactor App Component and take Sidemenu component in use and wire it with menu button in NavigationBar

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -8,25 +8,46 @@ import './styles/globals/main.global.css';
 import { WebSocketClient } from "./common/websocket-client";
 
 import { AppRoutes } from "./routes";
-import { NavigationBar } from "./components/navigation-bar"
+import { NavigationBar } from "./components/navigation-bar";
+import { Sidemenu } from "./components/sidemenu";
 
 const webSocketClient = new WebSocketClient('ws://' + document.location.host);
 webSocketClient.subscribeAction('system-monitor', (data) => console.log("Got data", data));
 
-const handleMenuButtonClick = (evt: React.MouseEvent<HTMLButtonElement>) => {
+interface OwnState {
+    sidemenuVisible: boolean;
+}
+
+class App extends React.Component<{}, OwnState> {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sidemenuVisible: false
+        }
+
+        this._handleMenuButtonClick = this._handleMenuButtonClick.bind(this);
+    }
+
+    public render(): JSX.Element {
+        return (
+            <BrowserRouter>
+                <div>
+                    <NavigationBar brand="HomeDashboard" onMenuButtonClick={this._handleMenuButtonClick} />
+                    <Sidemenu visible={this.state.sidemenuVisible} />
+                    <div className="page-wrapper">
+                        <AppRoutes />
+                    </div>
+                </div>
+            </BrowserRouter>
+        )
+    }
+
+    private _handleMenuButtonClick = (evt: React.MouseEvent<HTMLButtonElement>) => {
+        this.setState({ sidemenuVisible: true })
+    }
 
 }
 
-ReactDOM.render(
-    (
-        <BrowserRouter>
-            <div>
-                <NavigationBar brand="HomeDashboard" onMenuButtonClick={handleMenuButtonClick} />
-                <div className="page-wrapper">
-                    <AppRoutes />
-                </div>
-            </div>
-        </BrowserRouter>
-    ),
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
