5117fc8e0d4bbfcf99c5cdbbd0e5bb2c9395c415
Jokke Liikkanen <quutticode@gmail.com>
2017-10-16 00:01:14 +0300

    Add gauges for raspberry to the DashboardView with dummy data

--- client/src/app/views/dashboard/dashboard.view.tsx
+++ client/src/app/views/dashboard/dashboard.view.tsx
@@ -1,9 +1,43 @@
 import * as React from "react";
 
+import { Card } from "../../components/card";
+import { GaugeChart } from "../../components/gauge-chart";
+
+const styles: any = require("./dashboard.view.css");
+
 export class DashboardView extends React.Component<{}, {}> {
 
     public render(): JSX.Element {
-        return <h1>Dashboard</h1>
+        return (
+            <div className={styles.dashboardContainer}>
+                <h1>RaspHome</h1>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-3 col-xl-2">
+                            <Card heading="Memory usage">
+                                <GaugeChart min={0} max={1024} value={582} warningLevel={770} errorLevel={900} valueTextBuilder={(value) => `${value} mb`} />
+                            </Card>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-3 col-xl-2">
+                            <Card heading="Storage">
+                                <GaugeChart min={0} max={32} value={6.02} warningLevel={24} errorLevel={29} valueTextBuilder={(value) => `${value.toFixed(2)} gb`} />
+                            </Card>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-3 col-xl-2">
+                            <Card heading="CPU load">
+                                <GaugeChart min={0} max={100} value={70.01} warningLevel={70} errorLevel={95} valueTextBuilder={(value => `${value.toFixed(2)} %`)} />
+                            </Card>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-3 col-xl-2">
+                            <Card heading="CPU temp">
+                                <GaugeChart min={30} max={80} value={76.1} warningLevel={60} errorLevel={75} valueTextBuilder={(value) => `${value.toFixed(2)} C`} />
+                            </Card>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
     }
 
 }
