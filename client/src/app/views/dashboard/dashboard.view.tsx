4eb0fb099c84db630fe550faca5f679e627dfe85
Jokke Liikkanen <quutticode@gmail.com>
2017-10-16 22:35:23 +0300

    Apply DashboardView to use Grid components

--- client/src/app/views/dashboard/dashboard.view.tsx
+++ client/src/app/views/dashboard/dashboard.view.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Card } from "../../components/card";
 import { GaugeChart } from "../../components/gauge-chart";
+import { GridContainer, GridRow, GridCol } from "../../components/grid";
 
 const styles: any = require("./dashboard.view.css");
 
@@ -10,34 +11,34 @@ export class DashboardView extends React.Component<{}, {}> {
     public render(): JSX.Element {
         return (
             <div className={styles.dashboardContainer}>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-4">
+                <GridContainer>
+                    <GridRow verticalMiddle={true}>
+                        <GridCol xl={4}>
                             <h1>RaspHome</h1>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2">
+                        </GridCol>
+                        <GridCol lg={3} xl={2} md={6} sm={6}>
                             <Card heading="Memory usage">
                                 <GaugeChart min={0} max={1024} value={582} warningLevel={770} errorLevel={900} valueTextBuilder={(value) => `${value}`} unitText="mb" />
                             </Card>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2">
+                        </GridCol>
+                        <GridCol lg={3} xl={2} md={6} sm={6}>
                             <Card heading="Storage">
                                 <GaugeChart min={0} max={32} value={6.02} warningLevel={24} errorLevel={29} valueTextBuilder={(value) => `${value.toFixed(2)}`} unitText="gb" />
                             </Card>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2">
+                        </GridCol>
+                        <GridCol lg={3} xl={2} md={6} sm={6}>
                             <Card heading="CPU load">
                                 <GaugeChart min={0} max={100} value={70.01} warningLevel={70} errorLevel={95} valueTextBuilder={(value => `${value.toFixed(2)}`)} unitText="%" />
                             </Card>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-3 col-xl-2">
+                        </GridCol>
+                        <GridCol lg={3} xl={2} md={6} sm={6}>
                             <Card heading="CPU temp">
                                 <GaugeChart min={30} max={80} value={76.1} warningLevel={60} errorLevel={75} valueTextBuilder={(value) => `${value.toFixed(2)}`} unitText="'C" />
                             </Card>
-                        </div>
+                        </GridCol>
 
-                    </div>
-                </div>
+                    </GridRow>
+                </GridContainer>
             </div>
         )
     }
